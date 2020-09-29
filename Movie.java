39a9d54184b3566c5fc02356270325a9b568a7e6
dhee9000 <dhee9000@gmail.com>
2020-09-28 20:17:12 -0500

    added comments

--- Movie.java
+++ Movie.java
@@ -1,5 +1,17 @@
+/**
+ * Movie.java
+ * 
+ * Movie Object
+ * 
+ * <P>Stores information about instances of movies.
+ * 
+ */
+
 public class Movie {
 
+    /**
+     * Constants for _priceCode
+     */
     public static final int CHILDRENS   = 2;
     public static final int REGULAR     = 0;
     public static final int NEW_RELEASE = 1;
@@ -7,19 +19,36 @@ public class Movie {
     private String _title;
     private int    _priceCode;
     
+    /**
+     * Constructor
+     * @param title (required) title of the movie.
+     * @param priceCode (required) priceCode associated with the movie
+     */
     public Movie(String title, int priceCode) {
         _title = title;
         _priceCode = priceCode;
     }
     
+    /**
+     * Returns price code associated with the movie.
+     * @return priceCode associated with the movie.
+     */
     public int getPriceCode() {
         return _priceCode;
     }
     
-    public void setPriceCode(int arg) {
-        _priceCode = arg;
+    /**
+     * Sets the price code of a movie.
+     * @param newPriceCode (required) the price code to be set on this movie.
+     */
+    public void setPriceCode(int newPriceCode) {
+        _priceCode = newPriceCode;
     }
     
+    /**
+     * Returns the title of the movie
+     * @return title of the movie.
+     */
     public String getTitle() {
         return _title;
     }
--- Rental.java
+++ Rental.java
@@ -1,16 +1,38 @@
+/**
+ * Rental.java
+ * 
+ * Movie Rental Object
+ * 
+ * <P> Stores information about an instance of a movie rental.
+ * 
+ */
+
 public class Rental {
     private Movie _movie;
     private int   _daysRented;
     
+    /**
+     * Constructor
+     * @param movie (required) movie object associated with rental.
+     * @param daysRented (required) number of days movie object was checked out for.
+     */
     public Rental(Movie movie, int daysRented) {
         _movie      = movie;
         _daysRented = daysRented;
     }
     
+    /**
+     * Returns number of days the movie was rented for.
+     * @return daysRented for this instance of Rental.
+     */
     public int getDaysRented() {
         return _daysRented;
     }
     
+     /**
+      * Returns the Movie associated with the Rental.
+      * @return Movie associated with this Rental.
+      */
     public Movie getMovie() {
         return _movie;
     }
