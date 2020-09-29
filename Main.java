1fd6463c6c1793f3e2e784577d600c68a03e8fde
raviashwin <raviashwin@users.noreply.github.com>
2020-09-28 22:37:23 -0500

    main method

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Movie childrenMovie = new ChildrensMovie("Up");
+        Movie regularMovie = new RegularMovie("Transformers");
+        Movie newMovie = new NewMovie("Tenet");
+
+        Rental firstRental = new Rental(childrenMovie, 9);
+        Rental secondRental = new Rental(regularMovie, 46);
+        Rental thirdRental = new Rental(newMovie, 3);
+
+        Customer person = new Customer("Brad");
+
+        person.addRental(firstRental);
+        person.addRental(secondRental);
+        person.addRental(thirdRental);
+
+        System.out.println(Customer.generateStatement);
+
+
     }
 }
